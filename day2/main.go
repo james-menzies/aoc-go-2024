36ba29d993ed554e92fd8201f049e8cf5645f91d
James Menzies <james.r.menzies@gmail.com>
2024-12-08 11:26:18 +1100

    progress on day 2/3

--- day2/main.go
+++ day2/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/james-menzies/aoc-template-go/internal"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -10,9 +12,82 @@ func main() {
 	fmt.Printf("First Solution: %v, Second Solution: %v", solution1(input), solution2(input))
 }
 
+func parseNums(input []string) ([][]int, error) {
+	result := make([][]int, 0)
+
+	for lineNum, line := range input {
+		tokens := strings.Fields(line)
+
+		nums := make([]int, 0)
+		for tokenNum, token := range tokens {
+			tokenAsInt, err := strconv.Atoi(token)
+			if err != nil {
+				return nil, fmt.Errorf("line #%v pos #%v is not an integer: %v", lineNum, tokenNum, token)
+			}
+
+			nums = append(nums, tokenAsInt)
+		}
+
+		result = append(result, nums)
+	}
+
+	return result, nil
+}
+
+func isDiffSafe(a, b int, ascending bool) bool {
+	var diff int
+	if ascending {
+		diff = b - a
+	} else {
+		diff = a - b
+	}
+
+	return diff >= 1 || diff <= 3
+}
+
+func isReportSafe(report []int) bool {
+	var safetyUsed bool
+	var ascending bool = true
+	if report[0] > report[1] {
+		ascending = false
+	}
+
+	var prev int
+	for i := 1; i < len(report); i++ {
+		if !isDiffSafe(prev, report[i], ascending) {
+			if safetyUsed {
+				return false
+			}
+
+			if
+
+
+
+
+		} else {
+			prev = report[i]
+		}
+
+	}
+
+	return true
+}
+
 func solution1(input []string) any {
-	// TODO solve first challenge
-	return nil
+	var numberOfSafeReports int
+	reports, err := parseNums(input)
+	if err != nil {
+		panic(fmt.Errorf("could not parse AOC input: %w", err))
+	}
+
+	for _, report := range reports {
+		if isReportSafe(report) {
+			numberOfSafeReports++
+			fmt.Printf("report %+v is safe\n", report)
+		}
+	}
+
+	return numberOfSafeReports
 }
 
 func solution2(input []string) any {
--- day3/main.go
+++ day3/main.go
@@ -3,19 +3,60 @@ package main
 import (
 	"fmt"
 	"github.com/james-menzies/aoc-template-go/internal"
+	"regexp"
+	"strconv"
+	"strings"
 )
+
+var mulRegex = regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+var doRegex = regexp.MustCompile(`do\(\)`)
+var dontRegex = regexp.MustCompile(`don't\(\)`)
 
 func main() {
 	input := internal.ReadInput("day3/input.txt")
 	fmt.Printf("First Solution: %v, Second Solution: %v", solution1(input), solution2(input))
 }
 
+func calculateMulExpression(exp string) int {
+	stripped := exp[4 : len(exp)-1]
+	tokens := strings.Split(stripped, ",")
+	a, _ := strconv.Atoi(tokens[0])
+	b, _ := strconv.Atoi(tokens[1])
+	return a * b
+}
+
 func solution1(input []string) any {
-	// TODO solve first challenge
-	return nil
+
+	mulExpressions := make([]string, 0)
+	for _, line := range input {
+		mulExpressions = append(mulExpressions, mulRegex.FindAllString(line, -1)...)
+	}
+
+	var result int
+	for _, exp := range mulExpressions {
+		result += calculateMulExpression(exp)
+	}
+
+	return result
 }
 
 func solution2(input []string) any {
-	// TODO solve second challenge
+
+	mulExpressions := make([]string, 0)
+
+	for _, line := range input {
+		var from int
+		var to int
+		var currentDo int
+
+		dos := doRegex.FindAllStringIndex(line, -1)
+		donts := dontRegex.FindAllStringIndex(line, -1)
+
+		for _, dont := range donts {
+
+		}
+
+	}
+
 	return nil
 }
